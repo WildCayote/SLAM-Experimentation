2782c0332ca019f0ac29c662b4c614024c074417
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-15 14:38:17 +0300

    Feat: Setup the world in the main script

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+from environment import RobotEnvironment
+
+
+# create a world
+world = RobotEnvironment(
+    dimensions=(700, 1400),
+    world_path='./maps/map-1.png'
+)
+
+
+
+# setup pygame
+running = True
+
+while running:
+    # poll for event
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
